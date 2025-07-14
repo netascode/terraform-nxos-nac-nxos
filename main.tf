0943aeba4dd46df18057dde6a19b254119f02c06
Tobi Wang (haifwang) <haifwang@cisco.com>
2025-07-14 14:18:21 +0800

    fix variables interpolation with precedence: device-level > interface_group-level > device_group-devel > global

--- main.tf
+++ main.tf
@@ -62,11 +62,23 @@ locals {
     }
   }
 
+  interface_group_variables = {
+    for device in local.devices : device.name => {
+      for ig in local.interface_groups : ig.name => merge(concat(
+        [try(local.global.variables, {})],
+        [for dg in local.device_groups : try(dg.variables, {}) if contains(try(device.device_groups, []), dg.name)],
+        [for dg in local.device_groups : try(dg.variables, {}) if contains(try(dg.devices, []), device.name)],
+        [try(ig.variables, {})],
+        [try(device.variables, {})]
+      )...)
+    }
+  }
+
   interface_group_config = {
     for device in local.devices : device.name => [
       for ig in local.interface_groups : {
         name = ig.name
-        configuration = yamldecode(templatestring(local.interface_groups_raw_config[device.name][ig.name], local.device_variables[device.name]))
+        configuration = yamldecode(templatestring(local.interface_groups_raw_config[device.name][ig.name], local.interface_group_variables[device.name][ig.name]))
       }
     ]
   }
