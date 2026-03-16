96a0c8ffa68479350b845a2f557d766f8ffb441a
danischm <danischm@cisco.com>
2026-03-16 20:11:04 +0100

    Fix bridge domain bool values

--- nxos_bridge_domain.tf
+++ nxos_bridge_domain.tf
@@ -7,7 +7,7 @@ resource "nxos_bridge_domain" "bridge_domain" {
   bridge_domains = { for vlan in try(local.device_config[each.key].vlans, []) : "vlan-${vlan.id}" => {
     access_encap = try(vlan.vni, local.defaults.nxos.devices.configuration.vlans.vni, null) != null ? "vxlan-${try(vlan.vni, local.defaults.nxos.devices.configuration.vlans.vni)}" : null
     name         = try(vlan.name, local.defaults.nxos.devices.configuration.vlans.name, null)
-    admin_state  = try(vlan.admin_state, local.defaults.nxos.devices.configuration.vlans.admin_state, null)
+    admin_state  = try(vlan.admin_state, local.defaults.nxos.devices.configuration.vlans.admin_state, null) == null ? null : try(vlan.admin_state, local.defaults.nxos.devices.configuration.vlans.admin_state) ? "active" : "suspend"
     bridge_mode  = try(vlan.bridge_mode, local.defaults.nxos.devices.configuration.vlans.bridge_mode, null)
     control = join(",", sort(compact([
       try(vlan.policy_enforced, local.defaults.nxos.devices.configuration.vlans.policy_enforced, false) ? "policy-enforced" : "",
@@ -22,10 +22,10 @@ resource "nxos_bridge_domain" "bridge_domain" {
       try(vlan.forwarding_mode_route, local.defaults.nxos.devices.configuration.vlans.forwarding_mode_route, false) ? "route" : "",
     ])))
     long_name           = try(vlan.long_name, local.defaults.nxos.devices.configuration.vlans.long_name, null)
-    mac_packet_classify = try(vlan.mac_packet_classify, local.defaults.nxos.devices.configuration.vlans.mac_packet_classify, null)
+    mac_packet_classify = try(vlan.mac_packet_classify, local.defaults.nxos.devices.configuration.vlans.mac_packet_classify, null) == null ? null : try(vlan.mac_packet_classify, local.defaults.nxos.devices.configuration.vlans.mac_packet_classify) ? "enable" : "disable"
     mode                = try(vlan.mode, local.defaults.nxos.devices.configuration.vlans.mode, null)
     vrf_name            = try(vlan.vrf, local.defaults.nxos.devices.configuration.vlans.vrf, null)
-    cross_connect       = try(vlan.cross_connect, local.defaults.nxos.devices.configuration.vlans.cross_connect, null)
+    cross_connect       = try(vlan.cross_connect, local.defaults.nxos.devices.configuration.vlans.cross_connect, null) == null ? null : try(vlan.cross_connect, local.defaults.nxos.devices.configuration.vlans.cross_connect) ? "enable" : "disable"
   } }
 
   depends_on = [
