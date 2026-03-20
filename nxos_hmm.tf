7f982ef47a40e5290bd29f698ba72271d9eaf0e8
danischm <danischm@cisco.com>
2026-03-20 15:51:01 +0100

    Move fabric forwarding model

--- nxos_hmm.tf
+++ nxos_hmm.tf
@@ -1,14 +1,14 @@
 resource "nxos_hmm" "hmm" {
   for_each = { for device in local.devices : device.name => device
-    if try(local.device_config[device.name].system.fabric_forwarding, local.defaults.nxos.devices.configuration.system.fabric_forwarding, null) != null ||
+    if try(local.device_config[device.name].fabric_forwarding, local.defaults.nxos.devices.configuration.fabric_forwarding, null) != null ||
   length([for int in try(local.device_config[device.name].interfaces.vlans, []) : int if try(int.fabric_forwarding_mode, local.defaults.nxos.devices.configuration.interfaces.vlans.fabric_forwarding_mode, null) != null]) > 0 }
   device                  = each.key
   admin_state             = "enabled"
   instance_admin_state    = "enabled"
-  anycast_mac             = try(local.device_config[each.key].system.fabric_forwarding.anycast_gateway_mac, local.defaults.nxos.devices.configuration.system.fabric_forwarding.anycast_gateway_mac, null)
-  administrative_distance = try(local.device_config[each.key].system.fabric_forwarding.distance, local.defaults.nxos.devices.configuration.system.fabric_forwarding.distance, null)
-  limit_vlan_mac          = try(local.device_config[each.key].system.fabric_forwarding.limit_vlan_mac, local.defaults.nxos.devices.configuration.system.fabric_forwarding.limit_vlan_mac, null)
-  selective_host_probe    = try(local.device_config[each.key].system.fabric_forwarding.selective_host_probe, local.defaults.nxos.devices.configuration.system.fabric_forwarding.selective_host_probe, false) ? "yes" : "no"
+  anycast_mac             = try(local.device_config[each.key].fabric_forwarding.anycast_gateway_mac, local.defaults.nxos.devices.configuration.fabric_forwarding.anycast_gateway_mac, null)
+  administrative_distance = try(local.device_config[each.key].fabric_forwarding.distance, local.defaults.nxos.devices.configuration.fabric_forwarding.distance, null)
+  limit_vlan_mac          = try(local.device_config[each.key].fabric_forwarding.limit_vlan_mac, local.defaults.nxos.devices.configuration.fabric_forwarding.limit_vlan_mac, null)
+  selective_host_probe    = try(local.device_config[each.key].fabric_forwarding.selective_host_probe, local.defaults.nxos.devices.configuration.fabric_forwarding.selective_host_probe, false) ? "yes" : "no"
   interfaces = { for int in try(local.device_config[each.key].interfaces.vlans, []) : "vlan${int.id}" => {
     admin_state = "enabled"
     mode        = try(int.fabric_forwarding_mode, local.defaults.nxos.devices.configuration.interfaces.vlans.fabric_forwarding_mode, null)
