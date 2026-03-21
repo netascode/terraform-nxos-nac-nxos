2be08f111f45663e1bf6161a6e3bda33d4560f66
Daniel Schmidt <79086712+danischm@users.noreply.github.com>
2026-03-21 15:27:05 +0100

    Update interface ospf model (#57)

--- nxos_physical_interface.tf
+++ nxos_physical_interface.tf
@@ -63,7 +63,7 @@ locals {
         ospfv3_cost                             = try(int.ospfv3.cost, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.cost, null)
         ospfv3_dead_interval                    = try(int.ospfv3.dead_interval, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.dead_interval, null)
         ospfv3_hello_interval                   = try(int.ospfv3.hello_interval, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.hello_interval, null)
-        ospfv3_network_type                     = try(int.ospfv3.network_type, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.network_type, null)
+        ospfv3_network_type                     = try(int.ospfv3.network, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.network, null)
         ospfv3_passive_interface                = try(int.ospfv3.passive_interface, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.passive_interface, null)
         ospfv3_priority                         = try(int.ospfv3.priority, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.priority, null)
         ospfv3_instance_id                      = try(int.ospfv3.instance_id, local.defaults.nxos.devices.configuration.interfaces.ethernets.ospfv3.instance_id, null)
