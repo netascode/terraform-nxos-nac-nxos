4b5f6aa5b571868ec6e4f2479b2bfc716cdeed1a
danischm <danischm@cisco.com>
2026-05-06 14:14:08 +0200

    Bump provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     nxos = {
       source  = "CiscoDevNet/nxos"
-      version = "= 0.11.1"
+      version = "= 0.11.2"
     }
     utils = {
       source  = "netascode/utils"
