8dfcf0859f436e6f7c55585c70e234a1e9529bc9
Daniel Schmidt <79086712+danischm@users.noreply.github.com>
2026-05-26 19:58:01 +0200

    Vlans cli alignments (#159)

--- nxos_bridge_domain.tf
+++ nxos_bridge_domain.tf
@@ -1,7 +1,7 @@
 locals {
   bridge_domain_mode_map = {
-    "ce"          = "CE"
-    "fabric-path" = "FabricPath"
+    "ce"         = "CE"
+    "fabricpath" = "FabricPath"
   }
 }
 
@@ -12,24 +12,18 @@ resource "nxos_bridge_domain" "bridge_domain" {
   device        = each.key
   svi_autostate = try(local.device_config[each.key].system.interface_vlan_autostate, null) == null ? null : try(local.device_config[each.key].system.interface_vlan_autostate) ? "enable" : "disable"
   bridge_domains = length(try(local.device_config[each.key].vlan.vlans, [])) > 0 ? { for vlan in try(local.device_config[each.key].vlan.vlans, []) : "vlan-${vlan.id}" => {
-    access_encap = try(vlan.vni, null) != null ? "vxlan-${try(vlan.vni)}" : null
+    access_encap = try(vlan.vn_segment, null) != null ? "vxlan-${try(vlan.vn_segment)}" : null
     name         = try(vlan.name, null)
     admin_state  = try(vlan.state_active, null) == null ? null : try(vlan.state_active) ? "active" : "suspend"
     bridge_mode  = try(vlan.bridge_mode, null)
     control = length(compact([
-      try(vlan.policy_enforced, false) ? "policy-enforced" : "",
+      try(vlan.policy_enforce_vlan, false) ? "policy-enforced" : "",
       try(vlan.untagged, false) ? "untagged" : "",
       ])) > 0 ? join(",", sort(compact([
-        try(vlan.policy_enforced, false) ? "policy-enforced" : "",
+        try(vlan.policy_enforce_vlan, false) ? "policy-enforced" : "",
         try(vlan.untagged, false) ? "untagged" : "",
     ]))) : null
-    forwarding_mode = length(compact([
-      try(vlan.forwarding_mode_bridge, false) ? "bridge" : "",
-      try(vlan.forwarding_mode_route, false) ? "route" : "",
-      ])) > 0 ? join(",", sort(compact([
-        try(vlan.forwarding_mode_bridge, false) ? "bridge" : "",
-        try(vlan.forwarding_mode_route, false) ? "route" : "",
-    ]))) : null
+    forwarding_mode     = null
     long_name           = try(vlan.long_name, null)
     mac_packet_classify = try(vlan.mac_packet_classify, null) == null ? null : try(vlan.mac_packet_classify) ? "enable" : "disable"
     mode                = try(local.bridge_domain_mode_map[try(vlan.mode)], null)
