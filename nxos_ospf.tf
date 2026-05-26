c63d75d7b21e055db16d6b0f3ebd8c4398f65c3d
danischm <danischm@cisco.com>
2026-05-26 10:35:58 +0200

    OSPF cli alignments
    Add value mapping for area authentication enum rename (message_digest
    to md5 for provider), add changelog fragment for OSPF area auth and
    capability_vrf_lite enum changes.

--- nxos_ospf.tf
+++ nxos_ospf.tf
@@ -93,7 +93,7 @@ resource "nxos_ospf" "ospf" {
           max_metric_startup_interval = try(proc.max_metric_router_lsa.on_startup, false) ? try(proc.max_metric_router_lsa.on_startup_timeout, null) : null
 
           areas = length(try(proc.areas, [])) > 0 ? { for area in try(proc.areas, []) : (can(tonumber(area.id)) ? format("%d.%d.%d.%d", floor(tonumber(area.id) / 16777216) % 256, floor(tonumber(area.id) / 65536) % 256, floor(tonumber(area.id) / 256) % 256, tonumber(area.id) % 256) : tostring(area.id)) => {
-            authentication_type = try(area.authentication, null)
+            authentication_type = try(area.authentication, null) == null ? null : { "simple" = "simple", "message_digest" = "md5" }[try(area.authentication)]
             cost                = try(area.default_cost, null)
             control = length(compact([
               try(area.nssa_no_redistribution, null) != null ? (try(area.nssa_no_redistribution, false) ? "" : "redistribute") : "",
@@ -165,7 +165,7 @@ resource "nxos_ospf" "ospf" {
         max_metric_startup_interval = try(vrf.max_metric_router_lsa.on_startup, false) ? try(vrf.max_metric_router_lsa.on_startup_timeout, null) : null
 
         areas = length(try(vrf.areas, [])) > 0 ? { for area in try(vrf.areas, []) : (can(tonumber(area.id)) ? format("%d.%d.%d.%d", floor(tonumber(area.id) / 16777216) % 256, floor(tonumber(area.id) / 65536) % 256, floor(tonumber(area.id) / 256) % 256, tonumber(area.id) % 256) : tostring(area.id)) => {
-          authentication_type = try(area.authentication, null)
+          authentication_type = try(area.authentication, null) == null ? null : { "simple" = "simple", "message_digest" = "md5" }[try(area.authentication)]
           cost                = try(area.default_cost, null)
           control = length(compact([
             try(area.nssa_no_redistribution, null) != null ? (try(area.nssa_no_redistribution, false) ? "" : "redistribute") : "",
