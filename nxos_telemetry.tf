2309bb5329c1c8eca2b1e0df1481099492d7244e
Daniel Schmidt <79086712+danischm@users.noreply.github.com>
2026-05-26 19:43:46 +0200

    Telemetry, logging, ntp cli alignments (#158)

--- nxos_telemetry.tf
+++ nxos_telemetry.tf
@@ -2,14 +2,14 @@ resource "nxos_telemetry" "telemetry" {
   for_each = { for device in local.devices : device.name => device
     if try(local.device_config[device.name].telemetry.batch_dme_events, null) != null ||
     try(local.device_config[device.name].telemetry.merge_subscriptions, null) != null ||
-    try(local.device_config[device.name].telemetry.destination_profile_vrf, null) != null ||
+    try(local.device_config[device.name].telemetry.destination_profile_use_vrf, null) != null ||
     length(try(local.device_config[device.name].telemetry.destination_groups, [])) > 0 ||
     length(try(local.device_config[device.name].telemetry.sensor_groups, [])) > 0 ||
   length(try(local.device_config[device.name].telemetry.subscriptions, [])) > 0 }
   device                  = each.key
   batch_dme_events        = try(local.device_config[each.key].telemetry.batch_dme_events, null)
   merge_subscriptions     = try(local.device_config[each.key].telemetry.merge_subscriptions, null)
-  destination_profile_vrf = try(local.device_config[each.key].telemetry.destination_profile_vrf, null)
+  destination_profile_vrf = try(local.device_config[each.key].telemetry.destination_profile_use_vrf, null)
   destination_groups = length(try(local.device_config[each.key].telemetry.destination_groups, [])) > 0 ? { for dg in try(local.device_config[each.key].telemetry.destination_groups, []) : dg.id => {
     destinations = length(try(dg.destinations, [])) > 0 ? { for dest in try(dg.destinations, []) : "${dest.ip_address};${dest.port}" => {
       encoding = try(dest.encoding, null)
